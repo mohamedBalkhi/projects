9d9f74f8139da0e4d63b16a2a244eda05321381f
mohamedBalkhi <83403516+mohamedBalkhi@users.noreply.github.com>
2021-12-07 17:34:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+    "use strict"
+    $('.slider').ripples({
+        dropRadius: 15,
+        perturbance: 0.01,
+        
+      });
+      var typed = new Typed('.text', { strings: ["<strong class=tog>Together</strong>", "<strong>For A better</strong>","<strong class='future';>Future</strong>"], typeSpeed:0.1,loop:true });
+});
